a743347e064094f2d66729e946894165b9e7fedf
mazirui2001 <454434635@qq.com>
2025-09-03 19:51:47 +0800

    initial update

---
+++ src/main/scala/main.scala
@@ -0,0 +1,21 @@
+import chisel3._
+import circt.stage.ChiselStage
+import chisel3.stage.ChiselOption
+object Main extends App {
+    var firtoolOptions = Array(
+        "-disable-all-randomization", 
+        "-strip-debug-info",
+        "-strip-fir-debug-info",
+        "-O=release",
+        "--ignore-read-enable-mem",
+        "--lower-memories",
+        "--lowering-options=noAlwaysComb, disallowPackedArrays, disallowLocalVariables, explicitBitcast, disallowMuxInlining, disallowExpressionInliningInPorts",
+        "-o=verilog/",
+        "-split-verilog",
+)
+    ChiselStage.emitSystemVerilogFile(
+        new CPU,
+        Array("-td", "build/"),
+        firtoolOpts = firtoolOptions,
+    )
+}
